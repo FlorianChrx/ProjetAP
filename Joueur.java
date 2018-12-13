66902f01e7f04b783d3b48cfc4c42fab58107e4e
FlorianChrx <florianchiro@hotmail.fr>
2018-12-13 15:23:35 +0100

    Revert "change arborescence"
    This reverts commit 211c8235469a56c0ab82a04c39b133001662c794.

---
+++ Joueur.java
@@ -0,0 +1,7 @@
+class Joueur {
+	String nom = "Inconnu";
+	int vie = 0;
+	int score = 0;
+	int serie = 0;
+	int tentative = 1;
+}
--- compile.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-for i in $(ls ./src | grep .java)
-do 
-	javac -cp ap.jar ./src/$i -d . -nowarn
-done
